a2c64ad28cdceba9ecf98bc2c6bb2e24032027f2
HOUPERT <yoann.houpert@yahoo.fr>
2020-10-06 09:42:49 +0200

    fix payload init nlp

--- linto-application-in/topic/nlp.js
+++ linto-application-in/topic/nlp.js
@@ -1,9 +1,9 @@
 const debug = require('debug')(`linto:skill:v2:core:application-in:topic:nlp`)
 
 module.exports = async function (topic, rawPayload, applicationAuthType) {
-  let isConnect = await this.authToken.isAuthEnableAndValidToken(payload, applicationAuthType)
   const payload = JSON.parse(rawPayload)
 
+  let isConnect = await this.authToken.isAuthEnableAndValidToken(payload, applicationAuthType)
   const [_clientCode, _channel, _sn, _etat, _type, _id] = topic.split('/')
   const output = `${_clientCode}/tolinto/${_sn}/nlp/file/${_id}`
 
